79914cb919488781fa30ebe3871429ce80a7feeb
Inquisitor <ssd_@ukr.net>
2012-03-14 11:51:37 +0200

    [sd2_mr0376] Why it broke the encoding??
    Signed-off-by: Inquisitor <ssd_@ukr.net>

--- sql_mr/mr00374_scriptdev2_game_event_stuff.sql
+++ sql_mr/mr00374_scriptdev2_game_event_stuff.sql
@@ -38,7 +38,7 @@ INSERT INTO script_texts (entry, content_default, TYPE, sound) VALUES
 
 DELETE FROM `script_texts` WHERE `entry` in (-1730001, -1730002, -1730003, -1730004);
 INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc8`, `type`, `comment`) VALUES
-(-1730001, 'Turkey Hunter!', 'Охотник на индеек!', 3, 'Pilgrim\'s Bounty - 10 Wild Turkeys killed'),
-(-1730002, 'Turkey Domination!', 'Повелитель индеек!', 3, 'Pilgrim\'s Bounty - 20 Wild Turkeys killed'),
-(-1730003, 'Turkey Slaughter!', 'Истребитель индеек!', 3, 'Pilgrim\'s Bounty - 30 Wild Turkeys killed'),
-(-1730004, 'TURKEY TRIUMPH!', 'ТРИУМФ НАД ИНДЕЙКАМИ!', 3, 'Pilgrim\'s Bounty - 40 Wild Turkeys killed');
+(-1730001, 'Turkey Hunter!', 'РћС…РѕС‚РЅРёРє РЅР° РёРЅРґРµРµРє!', 3, 'Pilgrim\'s Bounty - 10 Wild Turkeys killed'),
+(-1730002, 'Turkey Domination!', 'РџРѕРІРµР»РёС‚РµР»СЊ РёРЅРґРµРµРє!', 3, 'Pilgrim\'s Bounty - 20 Wild Turkeys killed'),
+(-1730003, 'Turkey Slaughter!', 'РСЃС‚СЂРµР±РёС‚РµР»СЊ РёРЅРґРµРµРє!', 3, 'Pilgrim\'s Bounty - 30 Wild Turkeys killed'),
+(-1730004, 'TURKEY TRIUMPH!', 'РўР РРЈРњР¤ РќРђР” РРќР”Р•Р™РљРђРњР!', 3, 'Pilgrim\'s Bounty - 40 Wild Turkeys killed');
