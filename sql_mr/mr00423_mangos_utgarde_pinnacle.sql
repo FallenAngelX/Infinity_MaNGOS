9dca8d4c6da1ea3d56faa3b2a6182764fdd0e394
Reamer <philipp.dallig@web.de>
2012-05-13 01:30:17 +0300

    Update sql_mr/mr00423_mangos_utgarde_pinnacle.sql

--- sql_mr/mr00423_mangos_utgarde_pinnacle.sql
+++ sql_mr/mr00423_mangos_utgarde_pinnacle.sql
@@ -28,14 +28,14 @@ INSERT INTO spell_script_target VALUES
 UPDATE creature_template SET creature_template.ScriptName ='npc_skadi_grauf', creature_template.AIName='' , creature_template.InhabitType =7 WHERE entry = 26893;
 UPDATE creature_template SET creature_template.InhabitType =7 WHERE entry = 30775;
 UPDATE gameobject_template SET gameobject_template.ScriptName = 'harpoon_Skadi' WHERE gameobject_template.entry IN (192175,192176,192177);
--- skadi harpoon loot for one summon mob
+-- harpoon loot for one summon mob
 UPDATE creature_template SET creature_template.lootid = 26692 WHERE creature_template.entry = 26692;
 UPDATE creature_template SET creature_template.lootid = 26692 WHERE creature_template.entry = 30819;
 DELETE FROM creature_loot_template WHERE creature_loot_template.entry = 26692;
 INSERT INTO creature_loot_template  (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount) VALUES
 (26692, 37372, 100, 0, 1, 1);
 
---Yimiron
+-- Yimiron
 UPDATE creature_template SET creature_template.ScriptName = 'npc_spirit_fount', creature_template.AIName='' WHERE entry = 27339;
 
 -- achievements
