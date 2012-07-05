c2274d7d4ff6ea8445cc006ca319835e3045e575
Xfurry <xfurry@scriptdev2.com>
2012-07-05 22:32:42 +0300

    [2616] Implement event script for General Rajaxx - Ruins of AQ
    Based on the work of Nitka
    
    Note for DB devs: the full army needs to be spawned in DB. See SD2 forum for details.

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -245,6 +245,7 @@ extern void AddSC_boss_buru();
 extern void AddSC_boss_kurinnaxx();
 extern void AddSC_boss_ossirian();
 extern void AddSC_boss_moam();
+extern void AddSC_boss_rajaxx();
 extern void AddSC_ruins_of_ahnqiraj();
 extern void AddSC_instance_ruins_of_ahnqiraj();
 extern void AddSC_boss_cthun();                             // temple_of_ahnqiraj
@@ -775,6 +776,7 @@ void AddScripts()
     AddSC_boss_kurinnaxx();
     AddSC_boss_ossirian();
     AddSC_boss_moam();
+    AddSC_boss_rajaxx();
     AddSC_ruins_of_ahnqiraj();
     AddSC_instance_ruins_of_ahnqiraj();
     AddSC_boss_cthun();                                     // temple_of_ahnqiraj
