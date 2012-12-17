57c9beb20d636844f8eddc4a9a749a6a2158073d
Venom <venom_only@yahoo.com>
2012-12-17 18:49:23 +0400

    [sd2_mr0541] The Conquest Pit - Quests EAI implement. close #1280

--- sql_mr/mr00541_mangos_zuldrak_quests.sql
+++ sql_mr/mr00541_mangos_zuldrak_quests.sql
@@ -173,3 +173,22 @@ INSERT into creature_ai_scripts VALUES
 (3002204,30022,0,0,100,1,14000,14000,20000,20000,11,55977,4,6,0,0,0,0,0,0,0,0,'Vladof- Cast Whirlwind'),
 (3002305,30022,6,0,100,0,0,0,0,0,26,12948,0,0,0,0,0,0,0,0,0,0,'Vladof- Complete Quest upon death'),
 (3002306,30022,0,0,100,0,100,100,0,0,11,50689,0,1,0,0,0,0,0,0,0,0,'Vladof- Cast Blood Presence');
+
+-- The Conquest Pit - Quests from Venom
+REPLACE INTO `creature_ai_scripts` VALUES ('2771556', '27715', '6', '0', '100', '0', '0', '0', '0', '0', '26', '12427', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Complete 12427');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '27715';
+
+REPLACE INTO `creature_ai_scripts` VALUES ('2771656', '27716', '6', '0', '100', '0', '0', '0', '0', '0', '26', '12428', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Complete 12428');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '27716';
+
+REPLACE INTO `creature_ai_scripts` VALUES ('2771756', '27717', '6', '0', '100', '0', '0', '0', '0', '0', '26', '12429', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Complete 12429');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '27717';
+
+REPLACE INTO `creature_ai_scripts` VALUES ('2771856', '27718', '6', '0', '100', '0', '0', '0', '0', '0', '26', '12430', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Complete 12430');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '27718';
+
+REPLACE INTO `creature_ai_scripts` VALUES ('2772756', '27727', '6', '0', '100', '0', '0', '0', '0', '0', '26', '12431', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Quest Complete 12431');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '27727';
+
+UPDATE `creature_template` SET `faction_A`=14 WHERE `entry`=27727;
+UPDATE `creature_template` SET `faction_H`=14 WHERE `entry`=27727;
