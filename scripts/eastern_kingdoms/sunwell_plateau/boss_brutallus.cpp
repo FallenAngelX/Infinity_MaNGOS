53334215bb95e4ff71b609fa7a437b08af85b3fe
Xfurry <xfurry@scriptdev2.com>
2013-04-28 15:30:15 +0300

    [2884] Fix crash for Brutallus
    Thanks Azelen for the feedback
    
    Close #60

--- scripts/eastern_kingdoms/sunwell_plateau/boss_brutallus.cpp
+++ scripts/eastern_kingdoms/sunwell_plateau/boss_brutallus.cpp
@@ -389,7 +389,7 @@ struct MANGOS_DLL_DECL boss_brutallusAI : public ScriptedAI, private DialogueHel
     void UpdateAI(const uint32 uiDiff)
     {
         // Update only the intro related stuff
-        if (m_pInstance->GetData(TYPE_BRUTALLUS) == SPECIAL)
+        if (m_pInstance && m_pInstance->GetData(TYPE_BRUTALLUS) == SPECIAL)
         {
             UpdateIntroEvent(uiDiff);
             return;
