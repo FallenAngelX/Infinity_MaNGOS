4605afe9d62cd13a6c41ddd7ac7be0cb43809210
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-03 18:06:32 +0200

    [2903] Update to CMaNGOS [12524]
    This requires and fixes compile for CMaNGOS [12524] - https://github.com/cmangos/mangos-wotlk/commit/d2fd4baf8d936
    
    Also use the generic text storage now to load the data and display the texts
    
    Conflicts:
    	ScriptMgr.cpp
    	sql/scriptdev2_script_full.sql
    	system/system.cpp

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MangosR2 2812+ / for CMaNGOS 12514+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MangosR2 2812+ / for CMaNGOS 12524+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
