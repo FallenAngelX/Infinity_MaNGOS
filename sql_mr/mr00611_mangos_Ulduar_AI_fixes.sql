bea59b9144799cbee2b421ab3ac89f56a96d8bbd
Ulduar <zonozz@bk.ru>
2013-09-15 20:57:58 +0500

    Forgotten Ulduar_AI_fixes

--- sql_mr/mr00611_mangos_Ulduar_AI_fixes.sql
+++ sql_mr/mr00611_mangos_Ulduar_AI_fixes.sql
@@ -51,3 +51,40 @@ INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`,
 DELETE FROM `creature_linking_template` WHERE `map` = 603 AND `master_entry` = 33288;
 INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES
 ('33988','603','33288','4112','0');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) values('129539','126685','15');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) values('129540','126686','15');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) values('129541','126687','15');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) values('129538','126688','15');
+
+-- Script id: 62940
+DELETE FROM dbscripts_on_spell WHERE id=62940;
+INSERT INTO dbscripts_on_spell VALUES 
+(62940,0,18,1000,0,0,0,0,0,0,0,0,0,0,0,0,'Despawn at Teleport');
+
+
+-- Script id: 33699
+DELETE FROM dbscripts_on_creature_death WHERE id=33699;
+INSERT INTO dbscripts_on_creature_death VALUES 
+(33699,0,15,63630,0,33699,100,0,0,0,0,0,0,0,0,0,'Cast Avenger at death');
+
+
+-- Creature id: 33699
+UPDATE `creature_template` SET `AIName` = 'EventAI', `ScriptName` = '' WHERE `entry` = '33699';
+DELETE FROM creature_ai_scripts WHERE creature_id=33699;
+INSERT INTO creature_ai_scripts VALUES 
+('3369901','33699','0','0','100','31','50000','60000','50000','60000','11','63528','0','34','0','0','0','0','0','0','0','0','Cast Super charge in Combat'),
+('3369902','33699','0','0','100','31','12000','13000','17000','20000','11','63541','3','0','0','0','0','0','0','0','0','0','');
+
+-- Creature id: 33755
+UPDATE creature_template SET AIName='EventAI' WHERE entry=33755;
+DELETE FROM creature_ai_scripts WHERE creature_id=33755;
+INSERT INTO creature_ai_scripts VALUES 
+('3375501','33755','4','0','100','6','0','0','0','0','11','63616','0','0','0','0','0','0','0','0','0','0','');
+
+-- Creature id: 33754
+UPDATE creature_template SET AIName='EventAI' WHERE entry=33754;
+DELETE FROM creature_ai_scripts WHERE creature_id=33754;
+INSERT INTO creature_ai_scripts VALUES 
+('3375401','33754','4','0','100','2','0','0','0','0','11','63610','0','0','0','0','0','0','0','0','0','0',''),
+('3375402','33754','4','0','100','4','0','0','0','0','11','63674','0','0','0','0','0','0','0','0','0','0','');
