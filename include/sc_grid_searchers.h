b71a187459e7e074ca3092efe205d2bb6b6d7e9d
Xfurry <xfurry@scriptdev2.com>
2013-10-06 00:37:22 +0300

    [2957] Allow grid searchers to exclude the source from the actual search
    This is useful when searching for a creature with same entry as the source

--- include/sc_grid_searchers.h
+++ include/sc_grid_searchers.h
@@ -35,7 +35,7 @@ struct ObjectDistanceOrderReversed : public std::binary_function<const WorldObje
 
 Unit* GetClosestAttackableUnit(Unit* pSource, float fMaxSearchRange);
 GameObject* GetClosestGameObjectWithEntry(WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
-Creature* GetClosestCreatureWithEntry(WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange, bool bOnlyAlive = true, bool bOnlyDead = false);
+Creature* GetClosestCreatureWithEntry(WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange, bool bOnlyAlive = true, bool bOnlyDead = false, bool bExcludeSelf = false);
 
 void GetGameObjectListWithEntryInGrid(std::list<GameObject*>& lList , WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
 void GetCreatureListWithEntryInGrid(std::list<Creature*>& lList, WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
