c3d66e34ab39ed44609bea9b276ce81f327f022e
mns <pgameserver@gmail.com>
2013-10-20 10:39:13 +0330

    [sd2_mr0633] Revert some instance conditions entry to YTDB.

--- sql_mr/mr00633_mangos_temporary.sql
+++ sql_mr/mr00633_mangos_temporary.sql
@@ -18,3 +18,15 @@ UPDATE `creature_template_addon` SET `auras` = '70385 70405' WHERE `entry` IN(37
 DELETE FROM `creature` WHERE `id` = 36658;
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
 (49051, 36658, 658, 3, 1, 0, 0,1018.38, 167.25, 628.157, 5.2709, 86400, 0, 0, 653828, 0, 0, 0);
+
+-- ---------- --
+-- 2013-10-20 --
+-- ---------- --
+-- sd2_mr0633
+SET @ConditionEntry := 1807;
+REPLACE INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES
+(@ConditionEntry    , 18, 0, 0), -- INSTANCE_CONDITION_ID_NORMAL_MODE
+(@ConditionEntry + 1, 18, 1, 0), -- INSTANCE_CONDITION_ID_HARD_MODE
+(@ConditionEntry + 2, 18, 2, 0), -- INSTANCE_CONDITION_ID_HARD_MODE_2
+(@ConditionEntry + 3, 18, 3, 0), -- INSTANCE_CONDITION_ID_HARD_MODE_3
+(@ConditionEntry + 4, 18, 4, 0); -- INSTANCE_CONDITION_ID_HARD_MODE_4
