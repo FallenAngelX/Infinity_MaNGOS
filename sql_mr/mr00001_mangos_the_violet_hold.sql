db5dbde41a8f528fca7efca58c5941d4923fe0e5
Undergarun <undergarun@gmail.com>
2014-06-09 07:36:38 +0200

    [sd2_mr0656] Fit to fix latest changes in creature_template. Some cleanup

--- sql_mr/mr00001_mangos_the_violet_hold.sql
+++ sql_mr/mr00001_mangos_the_violet_hold.sql
@@ -10,25 +10,20 @@ UPDATE `creature_template` SET `ScriptName`='boss_lavanthor' WHERE entry=29312;
 UPDATE `creature_template` SET `ScriptName`='boss_zuramat'WHERE entry=29314;
 UPDATE `creature_template` SET `ScriptName`='mob_zuramat_sentry' WHERE entry=29364;
 UPDATE `creature_template` SET `ScriptName`='boss_cyanigosa' WHERE entry=31134;
-UPDATE `creature_template` SET `ScriptName`='npc_azure_saboteur' WHERE entry=31079;
+UPDATE `creature_template` SET `ScriptName`='npc_azure_saboteur', AIName='' WHERE entry=31079;
 UPDATE `creature_template` SET `ScriptName`='mob_vh_dragons', AIName='' WHERE entry IN (30666,30668,30667,32191,30660,30695,30663,30661,30664,30662);
 
--- Lieuntenant Sinclari (30658)
-UPDATE `creature` SET `Spawntimesecs`=180 WHERE id=30658;
-
-
--- void sentry template (29364)
-
-UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry` = 29364;
-UPDATE `creature_template` SET `faction_A`=16,`faction_H`=16 WHERE `entry`=31518;
-UPDATE `creature_template` SET `AIName`='' WHERE `entry` IN (30695,31079,30666,30668,30667,32191,30660,30695,30663,30661,30664,30662,29395);
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16 WHERE `entry` = 29364;
+UPDATE `creature_template` SET `FactionAlliance` = 16, `FactionHorde` = 16 WHERE `entry` = 31518;
+UPDATE `creature_template` SET `AIName`='' WHERE `entry` IN (29395);
 
 -- triggers
-UPDATE `creature_template` SET flags_extra=flags_extra|128  WHERE entry IN (30857,30883,29326,30896);
+UPDATE `creature_template` SET ExtraFlags = ExtraFlags | 128  WHERE entry IN (30857,30883,29326,30896);
 
+DELETE FROM `spell_script_target` WHERE entry = 59474;
 INSERT IGNORE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
-(59474,1,31511);
+(59474, 1, 31511);
 
 -- immune masks (charm, fear, root, silence, sleep, snare, stun, freeze, knockout, polymorph, banish, shackle, horror, sapped)
-UPDATE `creature_template` SET mechanic_immune_mask=mechanic_immune_mask|1|16|64|256|512|1024|2048|4096|8192|65536|131072|524288|8388608|536870912
+UPDATE `creature_template` SET MechanicImmuneMask = MechanicImmuneMask |1|16|64|256|512|1024|2048|4096|8192|65536|131072|524288|8388608|536870912
 WHERE `entry` IN (29315,29395,29316,29313,29321,29266,29271,29312,29314,31134,31511,31514,31509,31508,31512,31507,31510,31515,31513,31506);
--- sql_mr/mr00165_mangos_instance_the_shattered_halls.sql
+++ sql_mr/mr00165_mangos_instance_the_shattered_halls.sql
@@ -1,13 +1,4 @@
 -- Instance The Shattered Halls
--- ------------------------
 
--- ------------
 -- Shattered Hand Executioner
--- ------------
-UPDATE `creature_template` SET `unit_flags` = 3 WHERE `entry` = 20585; -- adding not attackable flag, removed by script on boss death
-
--- ------------
--- Prisoner Officers
--- ------------
-UPDATE `creature_template` SET `npcflag` = 2 WHERE `entry` = 17296; -- gossip flag added after death of Shattered Hand Executioner
-UPDATE `creature_template` SET `npcflag` = 2 WHERE `entry` = 17290;
+UPDATE `creature_template` SET `UnitFlags` = 3 WHERE `entry` = 20585; -- adding not attackable flag, removed by script on boss death
