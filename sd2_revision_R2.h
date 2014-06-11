1e9e3a0edeae2cea91ae3ae6e19c31accfe6a1e7
Undergarun <undergarun@gmail.com>
2014-06-11 04:21:18 +0200

    [sd2_mr0658] Delete temporary file and fix Gun'Drak SQL

--- sd2_revision_R2.h
+++ sd2_revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_R2_H__
 #define __SD2_REVISION_R2_H__
- #define SD2_REVISION_R2 "0657"
+ #define SD2_REVISION_R2 "0658"
 #endif // __SD2_REVISION_R2_H__
--- sql_mr/mr00360_mangos_gundrak.sql
+++ sql_mr/mr00360_mangos_gundrak.sql
@@ -1,4 +1,4 @@
 -- Gundrak
-UPDATE creature_template SET unit_flags = unit_flags&~33600 WHERE `entry` IN (29307, 31365);
+UPDATE creature_template SET UnitFlags = UnitFlags &~ 33600 WHERE `entry` IN (29307, 31365);
 UPDATE creature_template SET `ScriptName` = 'mob_sladran_constrictor' WHERE `entry` = 29713;
 UPDATE creature_template SET `ScriptName` = 'mob_sladran_snake_wrap' WHERE `entry` = 29742;
